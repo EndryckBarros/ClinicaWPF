0f34c9267424fb006a474397e1e21ee2da3bd6b3
Endryck <endryck.willyan.ilt@gmail.com>
2017-11-30 21:43:13 -0200

    Adicionando classe model do Medico close #1

---
+++ ViewWPF/Models/Medico.cs
@@ -0,0 +1,15 @@
+﻿namespace ViewWPF.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Especialidade { get; set; }
+
+        public int UsuarioId { get; set; }
+    }
+}
