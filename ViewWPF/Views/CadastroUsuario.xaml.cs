7a464c4fff452a3d42cb4ae5bb5508d5e447a69c
Endrick <Endrick@DESKTOP-CQTE0GH>
2017-12-01 23:43:19 -0200

    Adicionando telas de Cadastro: Medico, Paciente e Usuario

---
+++ ViewWPF/Views/CadastroUsuario.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ViewWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for CadastrodeUsuario.xaml
+    /// </summary>
+    public partial class CadastrodeUsuario : Window
+    {
+        public CadastrodeUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
