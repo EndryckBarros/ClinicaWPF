9b3c1e07913c9b13102a58d5458f2c260d41fa93
Endrick <Endrick@DESKTOP-CQTE0GH>
2017-12-02 00:05:44 -0200

    Adicionando telas de Medicos e Pacientes cadastrados, para visualização

---
+++ ViewWPF/Views/MedicosCadastrados.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ViewWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MedicosCadastrados.xaml
+    /// </summary>
+    public partial class MedicosCadastrados : Window
+    {
+        public MedicosCadastrados()
+        {
+            InitializeComponent();
+        }
+    }
+}
